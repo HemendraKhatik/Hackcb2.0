29d27ed5bc2b5252c3867928c438a2a2d1b25ccf
AamChora <37813372+HemendraKhatik@users.noreply.github.com>
2019-10-20 06:27:08 +0530

    Add files via upload

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, session, render_template, request, url_for,redirect
+from flask import Flask, session, render_template, request, url_for,redirect, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -24,19 +24,35 @@ db = scoped_session(sessionmaker(bind=engine))
 def index():
 	return render_template("signup.html")
 
-@app.route("/details")
-def details():
+@app.route("/html")
+def html():
 	return render_template("details.html")
+
+@app.route("/github")
+def details():
+	return render_template("github.html")
+	
 @app.route("/signup",methods=["POST","GET"])
 def signup():
 	if request.method == "GET":
 		return render_template("signup.html")
 	email=request.form.get("email")
-	password=request.form.get("password")
+	check_email=db.execute("SELECT * FROM signup WHERE email=:email",
+		{"email":email}).fetchall()
+	# email validation 
+	if check_email:
+		flash("Email address already used!")
+		return redirect(request.url)
+	# password confirmation 
+	if request.form.get("password") != request.form.get("c_password"):
+		flash("Password does not match!")
+		return redirect(request.url)
+	password = request.form.get("c_password");	
 	db.execute("INSERT INTO signup(email,password) VALUES(:email,:password)",
 	{"email":email,"password":password})
 	db.commit()
 	db.close()
+	flash("Your account is created!")
 	return render_template("login.html")
 
 @app.route("/login",methods=["POST","GET"])
@@ -50,8 +66,9 @@ def login():
 	for q in query:
 		if q.email==email and q.password==password:
 			return render_template("home.html")
-	return render_template("signup.html")		
-	
+	flash("Invalid email or password!")
+	return redirect(request.url)				
+
 @app.route("/logout",methods=["GET"])
 def logout():
 	return render_template("index.html")
